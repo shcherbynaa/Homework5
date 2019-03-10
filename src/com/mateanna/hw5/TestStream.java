e7ed84a396d26d0f44a22aa6f5d8cf3e225e91eb
shcherbynaa <47137576+shcherbynaa@users.noreply.github.com>
2019-03-10 14:43:49 +0200

    Calculator added

--- src/com/mateanna/hw5/TestStream.java
+++ src/com/mateanna/hw5/TestStream.java
@@ -1,4 +1,45 @@
 package com.mateanna.hw5;
 
+import jdk.dynalink.Operation;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+
 public class TestStream {
+
+    private static Map<Character, BinaryOperator<Double>> operator = operations();
+
+    private static Map<Character, BinaryOperator<Double>> operations() {
+        BinaryOperator<Double> sum = (x, y) -> x + y;
+        BinaryOperator<Double> multiply = (x, y) -> x * y;
+        BinaryOperator<Double> divide = (x, y) -> x / y;
+        BinaryOperator<Double> substract = (x, y) -> x - y;
+        BinaryOperator<Double> root = (x, y) -> (x / (1 / y));
+        BinaryOperator<Double> power = (x, y) -> (Math.pow(x, (1 / y)));
+        BinaryOperator<Double> power117 = (x, y) -> ((Math.pow(((x + y) / x + 117), y)));
+
+        Map<Character, BinaryOperator<Double>> operatorMap = new HashMap<>();
+        operatorMap.put('+', sum);
+        operatorMap.put('*', multiply);
+        operatorMap.put('/', divide);
+        operatorMap.put('-', substract);
+        operatorMap.put('#', root);
+        operatorMap.put('^', power);
+        operatorMap.put('p', power117);
+
+
+        return operatorMap;
+    }
+
+    public static double calculate(double x, char operator, double y) throws NullPointerException {
+        System.out.println("begin calculate");
+        BinaryOperator<Double> op = operations().get(operator);
+        return op.apply(x, y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate(2, '+', 5));
+    }
 }
